58f5675df25f5d85a5e7a1493e2ce1cd578ba4cc
Renato Ribeiro <renatoribroman@gmail.com>
2017-12-19 00:41:14 -0200

    update game ui, add score, moves and more

--- src/board.js
+++ src/board.js
@@ -1,10 +1,10 @@
-import { pipe } from 'ramda'
+import { clone, pipe, equals, flatten } from 'ramda'
 import { LEFT, UP, RIGHT, DOWN } from './directions'
 
-const identity = board => [...board]
-const reverseRows = board => [...board].map(row => row.reverse())
+const identity = board => clone(board)
+const reverseRows = board => clone(board).map(row => row.reverse())
 const transpose = board =>
-  [...board][0].map((_, i) => [...board].map(row => row[i]))
+  clone(board)[0].map((_, i) => clone(board).map(row => row[i]))
 
 const parseInOut = {
   [LEFT]: { in: identity, out: identity },
@@ -16,17 +16,32 @@ const parseInOut = {
   },
 }
 
-// over board
-export const over = fn => board => fn([...board])
+export const mapTiles = fn => board =>
+  clone(board).map((row, x) => row.map((tile, y) => fn(tile, x, y)))
 
-// traverse tiles
-export const traverse = fn => board =>
-  board.map((row, x) => row.map((tile, y) => fn(tile, x, y)))
+export const eachTile = fn => board =>
+  clone(board).forEach((row, x) => row.forEach((tile, y) => fn(tile, x, y)))
 
-export const newTile = (value = 0, locked = false) => ({
+export const values = mapTiles(tile => tile.value)
+export const flatValues = pipe(values, flatten)
+
+export const newTile = ({ value = 0, locked = false, score = 0 } = {}) => ({
   value,
   locked,
+  score,
 })
+
+export const newBoard = (size = 4) => {
+  const emptyBoard = Array(size)
+    .fill()
+    .map(() =>
+      Array(size)
+        .fill()
+        .map(() => newTile())
+    )
+
+  return pipe(spawn, spawn)(emptyBoard)
+}
 
 export const move = (
   direction,
@@ -38,14 +53,13 @@ export const move = (
   const isFirstMove = rowIdx === 0 && tileIdx === 0 && round === 1
 
   // parse board if is first move
-  const copy = isFirstMove ? parse.in(board) : [...board]
-  const row = [...copy[rowIdx]]
+  const copy = isFirstMove ? parse.in(clone(board)) : clone(board)
+  const row = clone(copy[rowIdx])
   const tile = row[tileIdx]
   const prevTile = row[tileIdx - 1]
 
   if (!prevTile) {
     // do not have previous tile, keep the tile intact.
-    row[tileIdx] = { ...tile }
   } else if (
     tile.value > 0 &&
     prevTile.value === tile.value &&
@@ -54,15 +68,18 @@ export const move = (
   ) {
     // tile and previous tile is same number and both unlocked
     // time to merge together in previous tile
-    row[tileIdx - 1] = newTile(tile.value + prevTile.value, true)
-    row[tileIdx] = newTile()
+    row[tileIdx - 1] = newTile({
+      score: tile.value + prevTile.value + tile.score + prevTile.score,
+      value: tile.value + prevTile.value,
+      locked: true,
+    })
+    row[tileIdx] = newTile({})
   } else if (tile.value > 0 && prevTile.value === 0) {
     // previous tile is empty, time to move!
-    row[tileIdx - 1] = { ...tile }
-    row[tileIdx] = newTile()
+    row[tileIdx - 1] = newTile({ ...tile })
+    row[tileIdx] = newTile({})
   } else {
     // otherwise, we can't move it, keep the tile intact.
-    row[tileIdx] = { ...tile }
   }
 
   copy[rowIdx] = row
@@ -79,18 +96,13 @@ export const move = (
   }
 
   // parse out and unlock all tiles
-  return pipe(parse.out, traverse(tile => ({ ...tile, locked: false })))(copy)
-}
-
-export const replace = (x, y, tile) => board => {
-  const copy = [...board]
-  copy[x][y] = tile
-  return copy
+  return pipe(parse.out, mapTiles(tile => ({ ...tile, locked: false })))(copy)
 }
 
 export const emptyTiles = board => {
   const positions = []
-  traverse((tile, x, y) => {
+
+  eachTile((tile, x, y) => {
     if (tile.value === 0) positions.push({ x, y })
   })(board)
 
@@ -100,5 +112,33 @@ export const emptyTiles = board => {
 export const randomEmptyTile = board => {
   const positions = emptyTiles(board)
   const chosen = positions[Math.floor(Math.random() * positions.length)]
+
   return typeof chosen === 'undefined' ? false : chosen
+}
+
+export const canMoveTo = direction => board => {
+  const moved = move(direction)(clone(board))
+  return !equals(flatValues(board), flatValues(moved))
+}
+
+export const isGameOver = board => {
+  const directions = Object.values({ LEFT, UP, RIGHT, DOWN })
+  return !directions.some(dir => canMoveTo(dir)(board))
+}
+
+export const score = board => {
+  return flatten(clone(board)).reduce((sum, tile) => sum + tile.score, 0)
+}
+
+export const spawn = board => {
+  const cloned = clone(board)
+  const position = randomEmptyTile(cloned)
+  const value = Math.random() <= 0.1 ? 4 : 2
+
+  if (position) {
+    const { x, y } = position
+    cloned[x][y] = newTile({ value })
+  }
+
+  return cloned
 }
--- src/index.js
+++ src/index.js
@@ -1,44 +1,165 @@
+import { pipe } from 'ramda'
 import { h, render, Component, Text } from 'ink'
 import { LEFT, UP, RIGHT, DOWN } from './directions'
-import { move, newTile } from './board'
+import leftPad from 'left-pad'
 
-const testBoard = [[0, 2, 2, 0], [2, 4, 0, 0], [0, 0, 0, 2], [0, 0, 2, 2]]
-const newBoard = matrix => matrix.map(r => r.map(v => newTile(v)))
+import { move, newBoard, canMoveTo, isGameOver, score, spawn } from './board'
+import { setTimeout } from 'core-js/library/web/timers'
+
+const colors = {
+  0: '#222222',
+  2: '#ffffff',
+  4: '#ede0c8',
+  8: '#f2b179',
+  16: '#f59563',
+  32: '#f67b5f',
+  64: '#7986CB',
+  128: '#64B5F6',
+  256: '#AED581',
+  512: '#DCE775',
+  1024: '#FFF176',
+  2048: 'purple',
+  4096: '#FFD54F',
+  8192: '#E57373',
+}
+
+const newGameState = () => ({
+  board: newBoard(),
+  score: 0,
+  gameOver: false,
+  moves: 0,
+  scoreToast: '',
+})
+
+const Line = ({ children, p = 0 }) => (
+  <div>
+    {Array(p)
+      .fill(' ')
+      .join('')}
+    {children}
+  </div>
+)
+
+const Section = ({ title, children }) => (
+  <div>
+    <div>
+      <Line p={1}>
+        <Text hex="#44ff44" bold>
+          {title}
+        </Text>
+      </Line>
+    </div>
+    <span>{children}</span>
+  </div>
+)
+
+const Key = ({ key, children, p = 0 }) => (
+  <Line p={p}>
+    <Text bold>{key}</Text>: {children}
+  </Line>
+)
 
 class Game extends Component {
-  state = {
-    board: newBoard(testBoard),
-    lastCmd: '',
+  state = newGameState()
+
+  newGame = () => this.setState(newGameState())
+
+  move = direction => {
+    this.setState(({ board, moves }) => {
+      const update = {}
+
+      if (canMoveTo(direction)(board)) {
+        update.board = pipe(move(direction), spawn)(board)
+        update.score = score(update.board)
+        update.moves = moves + 1
+
+        const scoreDiff = update.score - score(board)
+        scoreDiff > 0 && this.toast('scoreToast', `+${scoreDiff}`)
+
+        if (isGameOver(update.board)) {
+          update.gameOver = true
+        }
+      }
+
+      return update
+    })
+  }
+
+  toasts = {}
+  toast = (key, value, time = 1000) => {
+    if (this.toasts[key]) {
+      clearTimeout(this.toasts[key])
+    }
+
+    this.setState({ [key]: value }, () => {
+      this.toasts[key] = setTimeout(() => {
+        this.setState({ [key]: '' })
+      }, time)
+    })
   }
 
   handleKeyPress = (ch, key = {}) => {
-    const direction = {
-      left: LEFT,
-      up: UP,
-      right: RIGHT,
-      down: DOWN,
-    }[key.name]
-
-    if (direction) {
-      this.setState(prevState => ({
-        lastCmd: key.name,
-        board: move(direction)(prevState.board),
-      }))
+    const commands = {
+      left: () => this.move(LEFT),
+      up: () => this.move(UP),
+      right: () => this.move(RIGHT),
+      down: () => this.move(DOWN),
+      n: () => this.newGame(),
     }
+
+    const command = commands[key.name]
+    command && command()
   }
 
   componentDidMount() {
     process.stdin.on('keypress', this.handleKeyPress)
   }
 
+  renderBoard = () =>
+    this.state.board.map((r, ri) => (
+      <Line key={ri} p={3}>
+        {r.map((v, ti) => (
+          <Text key={ti} bgHex={colors[v.value]} hex="#222222">
+            [{leftPad(v.value === 0 ? '' : v.value, 5)}]
+          </Text>
+        ))}
+      </Line>
+    ))
+
   render() {
-    const { board } = this.state
+    const { score, gameOver, scoreToast, moves } = this.state
 
     return (
-      <div>
-        {board.map(r => r.map(v => v.value).join(', ')).join('\n')}
-        {`\nCommand: ${this.state.lastCmd}`}
-      </div>
+      <span>
+        <Section title="Keys">
+          <Key p={3} key="[←] [→] [↑] [↓]">
+            move
+          </Key>
+          <Key p={3} key="[n]">
+            restart / new game
+          </Key>
+        </Section>
+        <Section title="Game State">
+          <Line p={3}>
+            <Text>Moves</Text>: <Text yellow>{moves}</Text>
+          </Line>
+          <Line p={3}>
+            <Text>Score</Text>: <Text green>{score}</Text>{' '}
+            <Text hex="#999999">{scoreToast}</Text>
+          </Line>
+          {gameOver && (
+            <span>
+              <br />
+              <Line p={3}>
+                <Text hex="#ffffff" bgHex="#ff0000">
+                  Game Over
+                </Text>
+              </Line>
+            </span>
+          )}
+        </Section>
+        <Section title="Game">{this.renderBoard()}</Section>
+      </span>
     )
   }
 }
